165075e8bb793ab0f5259906890a861027f315ed
kapoor-arjun <kaparjun199@gmail.com>
2023-08-17 21:10:19 -0700

    Added Head-of-a-Text-File-2.sh

---
+++ Head-of-a-Text-File-2.sh
@@ -0,0 +1,8 @@
+# Head of a Text File #2
+# Problem Statement: In this challenge, we practice using the head command to display the first n characters of a text file.
+                     #Display the first 20 characters of an input file.
+# https://www.hackerrank.com/challenges/text-processing-head-2/problem
+
+#!/bin/bash
+
+head -c 20
