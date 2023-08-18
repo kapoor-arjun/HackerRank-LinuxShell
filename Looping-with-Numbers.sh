851b70bb8e2358f192085db74ffa170d1a08cdee
kapoor-arjun <kaparjun199@gmail.com>
2023-08-17 20:46:19 -0700

    Added Looping-with-Numbers.sh

---
+++ Looping-with-Numbers.sh
@@ -0,0 +1,10 @@
+# Looping with Numbers
+# Problem Statement: Use a for loop to display the natural numbers from 1 to 50.
+# https://www.hackerrank.com/challenges/bash-tutorials---looping-with-numbers/problem
+
+#!/bin/bash
+
+for i in {1..50}
+do
+    echo "$i"
+done
