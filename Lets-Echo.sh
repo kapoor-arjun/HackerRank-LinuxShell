adcd14a1ccf3a8998a2018f80981788d66598c1a
kapoor-arjun <kaparjun199@gmail.com>
2023-08-17 20:36:56 -0700

    Added Lets-Echo.sh

---
+++ Lets-Echo.sh
@@ -0,0 +1,7 @@
+# Let's Echo
+# Problem Statement: Write a bash script that prints the string "HELLO".
+# https://www.hackerrank.com/challenges/bash-tutorials-lets-echo/problem
+
+#!/bin/bash
+
+echo "HELLO"
