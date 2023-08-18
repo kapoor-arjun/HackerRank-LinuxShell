2ef4bb437f64c536dbd14cbd17575ee8151a623d
kapoor-arjun <kaparjun199@gmail.com>
2023-08-17 20:43:05 -0700

    Added A-Personalized-Echo.sh

---
+++ A-Personalized-Echo.sh
@@ -0,0 +1,9 @@
+# A Personalized Echo
+# Problem Statement: Write a Bash script which accepts name as input and displays the greeting "Welcome (name)"
+# https://www.hackerrank.com/challenges/bash-tutorials---a-personalized-echo/problem
+
+#!/bin/bash
+
+read name
+
+echo "Welcome $name"
