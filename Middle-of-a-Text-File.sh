e234aba118f4b09038ace0a7add192d8a9d0d8a0
kapoor-arjun <kaparjun199@gmail.com>
2023-08-17 21:11:36 -0700

    Added Middle-of-a-Text-File.sh

---
+++ Middle-of-a-Text-File.sh
@@ -0,0 +1,7 @@
+# Middle of a Text File
+# Problem Statement: Display the lines (from line number 12 to 22, both inclusive) of a given text file.
+# https://www.hackerrank.com/challenges/text-processing-in-linux---the-middle-of-a-text-file/problem
+
+#!/bin/bash
+
+head -n 22 | tail -n 11
