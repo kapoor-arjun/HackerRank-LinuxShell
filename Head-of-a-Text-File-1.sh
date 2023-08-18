ea3acc32a9e50a61bb207995ff7e2568e15f508d
kapoor-arjun <kaparjun199@gmail.com>
2023-08-17 21:08:35 -0700

    Added Head-of-a-Text-File-1.sh

---
+++ Head-of-a-Text-File-1.sh
@@ -0,0 +1,8 @@
+# Head of a Text File #1
+# Problem Statement: In this challenge, we practice using the head command to display the first n lines of a text file.
+                     #Display the first 20 lines of an input file.
+# https://www.hackerrank.com/challenges/text-processing-head-1/problem
+
+#!/bin/bash
+
+head -20
