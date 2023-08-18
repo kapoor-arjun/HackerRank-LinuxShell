48aceb9289cbc901d814c79d03e591e92048780b
kapoor-arjun <kaparjun199@gmail.com>
2023-08-18 10:02:51 -0700

    Added Cut1-9.sh

---
+++ Cut1-9.sh
@@ -0,0 +1,61 @@
+# Cut-1
+# https://www.hackerrank.com/challenges/text-processing-cut-1/problem
+
+#!/bin/bash
+cut -b 3
+
+
+# Cut-2
+# https://www.hackerrank.com/challenges/text-processing-cut-2/problem
+
+#!/bin/bash
+cut -b 2,7
+
+
+# Cut-3
+# https://www.hackerrank.com/challenges/text-processing-cut-3/problem
+
+#!/bin/bash
+cut -b 2-7
+
+
+# Cut-4
+# https://www.hackerrank.com/challenges/text-processing-cut-4/problem
+
+#!/bin/bash
+cut -b 1-4
+
+
+# Cut-5
+# https://www.hackerrank.com/challenges/text-processing-cut-5/problem
+
+#!/bin/bash
+cut -f 1-3
+
+
+# Cut-6
+# https://www.hackerrank.com/challenges/text-processing-cut-6/problem
+
+#!/bin/bash
+cut -b 13-
+
+
+# Cut-7
+# https://www.hackerrank.com/challenges/text-processing-cut-7/problem
+
+#!/bin/bash
+cut -d ' ' -f4
+
+
+# Cut-8
+# https://www.hackerrank.com/challenges/text-processing-cut-8/problem
+
+#!/bin/bash
+cut -d ' ' -f 1-3
+
+
+# Cut-9
+# https://www.hackerrank.com/challenges/text-processing-cut-9/problem
+
+#!/bin/bash
+cut -f 2-
