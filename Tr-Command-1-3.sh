5e4bfe5b0ee348678f13ea05fb455d75e50f035b
kapoor-arjun <kaparjun199@gmail.com>
2023-08-21 07:47:10 -0700

    Added Tr-Command-1-3.sh

---
+++ Tr-Command-1-3.sh
@@ -0,0 +1,19 @@
+# 'Tr' Command #1
+# https://www.hackerrank.com/challenges/text-processing-tr-1/problem
+
+#!/bin/bash
+tr "()" "[]"
+
+
+# 'Tr' Command #2
+# https://www.hackerrank.com/challenges/text-processing-tr-2/problem
+
+#!/bin/bash
+tr -d [a-z]
+
+
+# 'Tr' Command #3
+# https://www.hackerrank.com/challenges/text-processing-tr-3/problem
+
+#!/bin/bash
+tr -s " "
