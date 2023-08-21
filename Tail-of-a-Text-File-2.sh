f1d71588b6cdd02402584cfbe04b1b8be38c7d24
kapoor-arjun <kaparjun199@gmail.com>
2023-08-21 07:44:23 -0700

    Added Tail-of-a-Text-File-2.sh

---
+++ Tail-of-a-Text-File-2.sh
@@ -0,0 +1,8 @@
+# Tail of a Text File #2
+# Problem Statement: In this challenge, we practice using the tail command to display the last n characters of a text file. 
+                     #Display the last 20 characters of an input file.
+# https://www.hackerrank.com/challenges/text-processing-tail-2/problem
+
+#!/bin/bash
+
+tail -c20
