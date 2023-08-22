dd6322c06e00c0f05b387eefe87bdd1048bccfe2
kapoor-arjun <kaparjun199@gmail.com>
2023-08-22 13:13:58 -0700

    Added Sort-Command-1-7.sh

---
+++ Sort-Command-1-7.sh
@@ -0,0 +1,47 @@
+# Sort Command #1
+# https://www.hackerrank.com/challenges/text-processing-sort-1/problem
+
+#!/bin/bash
+sort
+
+
+# Sort Command #2
+# https://www.hackerrank.com/challenges/text-processing-sort-2/problem
+
+#!/bin/bash
+sort -r
+
+
+# Sort Command #3
+# https://www.hackerrank.com/challenges/text-processing-sort-3/problem
+
+#!/bin/bash
+sort -V
+
+
+# Sort Command #4
+# https://www.hackerrank.com/challenges/text-processing-sort-4/problem
+
+#!/bin/bash
+sort -rn
+
+
+# Sort Command #5
+# https://www.hackerrank.com/challenges/text-processing-sort-5/problem
+
+#!/bin/bash
+sort -k2 -rn -t$'\t'
+
+
+# Sort Command #6
+# https://www.hackerrank.com/challenges/text-processing-sort-6/problem
+
+#!/bin/bash
+sort -k2 -n -t$'\t'
+
+
+# Sort Command #7
+# https://www.hackerrank.com/challenges/text-processing-sort-7/problem
+
+#!/bin/bash
+sort -k2 -rn -t$'|'
